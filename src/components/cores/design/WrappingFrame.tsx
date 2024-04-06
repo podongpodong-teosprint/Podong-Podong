03c567846172c18f2234f75edbe7b9cccf27a81a
summermong <2514o_o@naver.com>
2024-04-07 01:08:57 +0900

    [Fix] : Book 컴포넌트 수정 && Frame으로 Wrapping

---
+++ src/components/cores/design/WrappingFrame.tsx
@@ -0,0 +1,15 @@
+type WrapperProps = {
+  children: React.ReactNode;
+};
+
+const WrappingFrame = ({ children }: WrapperProps) => {
+  return (
+    <div className="flex justify-center bg-gray h-screen w-screen">
+      <div className="bg-white h-screen p-2" style={{ width: '375px' }}>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default WrappingFrame;
