9ad3515d34e5845e85323fd495384f6b048797a0
summermong <2514o_o@naver.com>
2024-04-06 23:41:27 +0900

    [Feat] : tailwind 및 css 종속성

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      purple: '#DCBFFF',
+      'purple-hover': '#D0A2F7',
+      'modal-bg-color': '#F1EAFF',
+      gray: '#EEEEEE',
+      white: '#ffffff',
+      green: '#A4E5A7',
+      'green-hover': '#6FCB73',
+    },
+    fontSize: {
+      small: '12px',
+      basic: '16px',
+      title: '48px',
+      'sub-title': '24px',
+    },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
 };
