988bb66c1993bbb45d173e8a0b6efdedd25c7d49
yeojincho0129 <yeojincho0129@gmail.com>
2024-04-07 20:51:37 +0900

    [Feat] : 빈 포도 모달 연결

--- src/pages/LibraryPage.tsx
+++ src/pages/LibraryPage.tsx
@@ -1,8 +1,12 @@
+import { useState, useRef } from 'react';
 import { twMerge } from 'tailwind-merge';
 import { TypeGrape, TypeGrapeShape } from '../components/podo/types';
 import { grapes, styleByType } from '../components/podo/consts';
 // import { TypeMemorySchema } from 'apis/memory';
 import ConfirmModal from '../components/library/ConfirmModal';
+import { FaSearch } from 'react-icons/fa';
+import Book from 'components/cores/design/Book';
+import { searchBook } from '../../api/bookAPI.js';
 
 export default function LibraryPage() {
   const podoList: PodoList[] = [
@@ -17,7 +21,7 @@ export default function LibraryPage() {
   return (
     <>
       <div className="">
-        <h2>포동포동의 서재</h2>
+        <h2 className="text-title">포동포동의 서재</h2>
         <ul className="flex space-x-5">
           <li>전체</li>
           <li>읽은 책</li>
@@ -73,8 +77,29 @@ export function Podo() {
 
 // 비어있는 포도
 export function EmptyPodo() {
+  const modalRef = useRef<HTMLDialogElement | null>(null);
+  const [searchTitle, setSearchTitle] = useState('');
+  const [apiBooks, setApiBooks] = useState([]);
+  const handleSearch = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    try {
+      if (e.key === 'Enter') {
+        const result = await searchBook(searchTitle);
+        setApiBooks(result);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const openModal = () => {
+    setApiBooks([]);
+    if (modalRef.current) {
+      modalRef.current.showModal();
+    }
+  };
+
   const handlePodoClick = () => {
-    alert('circle clicked');
+    openModal();
   };
 
   const createGrape = (shape: TypeGrapeShape, type: TypeGrape) => {
@@ -98,11 +123,54 @@ export function EmptyPodo() {
           fill="#14AE5C"
         />
         {grapes.map((grape, i) => {
-          return <circle id={`${i}`} key={i} {...createGrape(grape, 0)} fill="red" />;
+          return <circle id={`${i}`} key={i} {...createGrape(grape, 0)} fill="white" />;
         })}
       </svg>
       <div>제목</div>
       <ConfirmModal />
+      <dialog ref={modalRef} id="my_modal_1" className="p-3 modal">
+        <div className="w-full modal-box bg-gray">
+          <div className="flex items-center border-2 border-dashed border-gray bg-white rounded-full px-3 py-1 w-full">
+            <FaSearch className="mr-2 " />
+            <input
+              type="text"
+              placeholder="책 제목을 입력하세요"
+              className="flex-1 outline-none"
+              value={searchTitle}
+              onChange={(e) => setSearchTitle(e.target.value)}
+              onKeyUp={handleSearch}
+            />
+          </div>
+          <div>
+            <div>
+              {apiBooks.length ? (
+                <div className="my-3 overflow-y-auto h-80">
+                  {apiBooks.map((book) => (
+                    <Book
+                      title={book.title}
+                      authors={book.authors}
+                      publisher={book.publisher}
+                      thumbnail={book.thumbnail}
+                    />
+                  ))}
+                </div>
+              ) : (
+                <p className="text-center my-3 text-[20px]">책이 없습니다.</p>
+              )}
+            </div>
+          </div>
+          <div className="modal-action">
+            <form method="dialog" className="flex gap-1">
+              <button
+                className="px-3 py-1 rounded-full bg-purple hover:bg-purple-hover"
+                onClick={() => setSearchTitle('')}
+              >
+                닫기
+              </button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </div>
   );
 }
