09cdc5f38975ae1b64f91f0c259ff4d1f7a80828
Henry-Hong <hhr8540@naver.com>
2024-04-10 13:46:43 +0900

    [Hotfix] : useToday 훅 파싱에러 수정

--- src/hooks/useToday.ts
+++ src/hooks/useToday.ts
@@ -2,10 +2,14 @@ import { useMemo } from 'react';
 
 export const useToday = () => {
   const todayDate = useMemo(() => {
-    const date = new Date().toLocaleDateString().split('.').slice(0, -1);
+    const date = new Date()
+      .toLocaleDateString()
+      .split('.')
+      .slice(0, -1)
+      .map((e) => e.replaceAll(' ', '').padStart(2, '0'));
     const year = date[0];
-    const month = date[1].replace(' ', '0');
-    const day = date[2].replace(' ', '0');
+    const month = date[1];
+    const day = date[2];
     return `${year}-${month}-${day}`;
   }, []);
 
